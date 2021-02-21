9cd763a7aadc677a268493078aaf01ac04cc5aad
何喜 <877073978@qq.com>
2021-02-21 20:45:04 +0800

    登录板块功能实现完成

--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -11,13 +11,13 @@
         <span class="userLogin">用户登录</span>
       </div>
       <!-- element表单 -->
-      <el-form ref="form" :model="form">
+      <el-form ref="form" :model="form" :rules="rules">
         <!-- 用户电话号码 -->
-        <el-form-item class="loginPhone">
+        <el-form-item class="loginPhone" prop="phone">
           <el-input placeholder="请输入手机号" prefix-icon="el-icon-user" v-model="form.phone"></el-input>
         </el-form-item>
         <!-- 用户密码 -->
-        <el-form-item>
+        <el-form-item prop="password">
           <el-input
             placeholder="请输入密码"
             prefix-icon="el-icon-lock"
@@ -26,7 +26,7 @@
           ></el-input>
         </el-form-item>
         <!-- 验证码 -->
-        <el-form-item>
+        <el-form-item prop="code">
           <el-row>
             <el-col :span="16">
               <el-input placeholder="请输入验证码" prefix-icon="el-icon-key" v-model="form.code"></el-input>
@@ -37,8 +37,8 @@
           </el-row>
         </el-form-item>
         <!-- 协议 -->
-        <el-form-item>
-          <el-checkbox v-model="checked"></el-checkbox>
+        <el-form-item prop="checked">
+          <el-checkbox v-model="form.checked"></el-checkbox>
           <span class="agreement">
             我已阅读并同意
             <el-link type="primary">用户协议</el-link>和
@@ -47,11 +47,11 @@
         </el-form-item>
         <!-- 登录按钮-->
         <el-form-item>
-          <el-button class="enter" type="primary">登录</el-button>
+          <el-button @click="submit" class="enter" type="primary">登录</el-button>
         </el-form-item>
         <!-- 注册按钮 -->
         <el-form-item>
-          <el-button class="register" type="primary">注册</el-button>
+          <el-button @click="reset" class="register" type="primary">注册</el-button>
         </el-form-item>
       </el-form>
     </div>
@@ -70,10 +70,57 @@ export default {
         phone: "",
         password: "",
         key: "",
+        checked: false,
       },
       baseUrl: "http://127.0.0.1/heimamm/public",
-      checked: false,
+      // 表单验证的规则
+      rules: {
+        phone: [
+          { required: true, message: "手机号不能为空", trigger: "blur" },
+          {
+            pattern: /^(?:(?:\+|00)86)?1(?:(?:3[\d])|(?:4[5-79])|(?:5[0-35-9])|(?:6[5-7])|(?:7[0-8])|(?:8[\d])|(?:9[189]))\d{8}$/,
+            message: "手机号不符合要求",
+            trigger: "blur",
+          },
+        ],
+        password: [
+          {
+            required: true,
+            message: "密码不能为空",
+            trigger: "blur",
+          },
+        ],
+        code: [
+          {
+            required: true,
+            message: "验证码不能为空",
+            trigger: "blur",
+          },
+        ],
+      },
     };
+  },
+  methods: {
+    //点击登录时需要进行自动验证
+    submit() {
+      //优先判断用户是否勾选了协议
+      if (!this.form.checked) {
+        this.$message.error("请先阅读并同意条款协议");
+        return;
+      }
+      //得到form对象
+      this.$refs.form.validate((valid) => {
+        if (valid) {
+          this.$message.success("登录成功");
+        } else {
+          this.$message.error("登录失败");
+        }
+      });
+    },
+    reset() {
+      //重置表单
+      this.$refs.form.resetFields();
+    },
   },
 };
 </script>
